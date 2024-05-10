9040526b3cc8e98094e9316bd8394aded42a1ea3
juyounglee <james289@naver.com>
2024-05-10 12:35:04 +0900

    [Rename] rename file name in stock folder - add modules for find_company_stock.py to rename file name in stock folder - add json file for company in stock folder - rename file name in stock folder issued_id : DE-40

--- find_company_stock.py
+++ find_company_stock.py
@@ -3,6 +3,7 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import quote
+import json
 
 def find_unmatched_company():
   loaded_company_list = get_loaded_company_list()
@@ -36,38 +37,52 @@ def get_company_list():
 
 def find_company_in_market():
   root_url = "https://finance.naver.com/search/search.naver?query="
-  company_in_stock = []
-  for company in extract_company_name():
-    encode_str = quote(company, encoding='euc-kr')
+  company_in_stock = {}
+  for trans_company, origin_company in extract_company_name().items():
+    encode_str = quote(trans_company, encoding='euc-kr')
     response = requests.get(root_url + encode_str, headers={'User-agent': 'Mozilla/5.0'})
     soup = BeautifulSoup(response.text, 'html.parser')
     if soup.select('.result_area > p > em') == []:
-      company_in_stock.append(company)
+      company_in_stock[trans_company] = origin_company
   return company_in_stock
 
 def extract_company_name():
-  company_list = []
+  company_list = {}
   for company in find_unmatched_company():
     if company[0] == '(':
-      company_list.append(company.split(')')[1].strip())
+      original_company = company
+      company_list[company.split(')')[1].strip()] = original_company
     elif company[-1] == ')':
-      company_list.append(company.split('(')[0].strip())
+      original_company = company
+      company_list[company.split('(')[0].strip()] = original_company
     elif company[-1] == '㈜':
-      company_list.append(company[:-1].strip())
+      original_company = company
+      company_list[company[:-1].strip()] = original_company
     elif company[0] == '㈜':
-      company_list.append(company[1:].strip())
+      original_company = company
+      company_list[company[1:].strip()] = original_company
     else:
-      company_list.append(company)
+      company_list[company] = company
   return company_list
 
-def save_to_csv(data, filename):
-    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        for row in data:
-            writer.writerow(row)
+def rename_files(directory, name_dict):
+    for filename in os.listdir(directory):
+        company_name = filename.split('_')[0]
+        company_num = filename.split('_')[1].split('.')[0]
+        if company_name in name_dict:
+            os.rename(os.path.join(directory, filename), os.path.join(directory, name_dict[company_name] + '_' + company_num + '.csv'))
+
+def save_dict_to_file(dict_obj, file_name):
+    with open(file_name, 'w') as file:
+        json.dump(dict_obj, file)
+
+def load_dict_from_file(file_name):
+    with open(file_name, 'r') as file:
+        return json.load(file)
 
 def main():
-  find_company_in_market()
+  name_dict = load_dict_from_file("company_in_stock.json")
+  rename_files("original_csv/stock/", name_dict)
 
 if __name__ == '__main__':
    main()
